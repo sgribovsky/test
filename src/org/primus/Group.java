7303bd91338ce3b7e4c383d2d5d24cdc833afeca
Sergey Gribovsky <sgribovsky@gmail.com>
2015-06-04 13:47:22 +0300

    Next version

---
+++ src/org/primus/Group.java
@@ -0,0 +1,4 @@
+package org.primus;
+
+public class Group {
+}
--- src/org/primus/Main.java
+++ src/org/primus/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Application is executed.");
+        System.out.println("!!!");
     }
 }
